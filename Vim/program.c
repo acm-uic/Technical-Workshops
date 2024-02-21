6d492fd27bef8b70c25d60d9ed53038b67960e98
Yashi Shandilya <yshand2@uic.edu>
2024-02-21 12:29:57 -0600

    Added file to help people create their very first program in Vim

--- Vim/program.c
+++ Vim/program.c
@@ -3,7 +3,6 @@
 int main(int argc, char **argv) {
 	printf("Welcome to ACM!\n");
 	printf("I am Yashi Shandilya\n");
-	printf("I am the President, Tech Lead, and SIG Lead for SIG: Jobs and AI\n");
-	
+	printf("I am the President, Tech Lead, and SIG Lead for SIG: Jobs and AI\n");	
 }
 
