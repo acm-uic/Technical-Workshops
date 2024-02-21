a162b2157b32e1940bbba9c7cdaddf0f4e88e316
Yashi Shandilya <yshand2@uic.edu>
2024-02-21 11:58:07 -0600

    Modified program.c to not be basic, includes introduction to the host and their positions.

--- Vim/program.c
+++ Vim/program.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-	printf("Hello World!\n");
+	printf("Welcome to ACM!\n");
+	printf("I am Yashi Shandilya\n");
+	printf("I am the President, Tech Lead, and SIG Lead for SIG: Jobs and AI\n");
+	
 }
+
